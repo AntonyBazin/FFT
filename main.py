7924599820219c9d1bf9e3ce6377bf768e1e07bf
Anton <antonyvbazin@gmail.com>
2020-11-21 18:09:35 +0300

    Some add-ons and refactoring

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
 from PIL import Image
 from scipy import fft
-import matplotlib
-
-matplotlib.use('TkAgg')
-
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 import numpy as np
 
 
-def save_new(pixels, filename, mark):
+def save_grayscale(image_name: str):
+    grayscale_image = Image.open(image_name).convert('L')
+    grayscale_image.save('./buffer/gray_' + image_name)
+    return './buffer/gray_' + image_name
+
+
+def save_new(pixels, filename, addition):
+    """Save a version of the image from the pixels array"""
     image = Image.fromarray(pixels, 'L')
-    image.save(mark + filename)
-    return image
+    image.save(addition + filename)
 
 
 def plot_spectrum(im_fft):
-    from matplotlib.colors import LogNorm
-    # A logarithmic colormap
-    plt.imshow(np.abs(im_fft), norm=LogNorm(vmin=5))
+    """A logarithmic colormap"""
+    plt.imshow(np.abs(im_fft), cmap='gray', norm=LogNorm(vmin=1))
     plt.colorbar()
 
 
 def run(filename: str):
-    data = plt.imread('./samples/' + filename) # np.asarray(img)  # 2d pixel array
+    gray_img = save_grayscale(filename)
+
+    data = plt.imread(gray_img)  # 2d pixel array
     plt.figure()
     plt.imshow(data, cmap='gray')
     plt.title('Original image')
 
-    new_image = fft.rfft2(data)
+    new_image = fft.fft2(data)  # may also use real fft here, but it will only give a half of the image
     plt.figure()
-    plt.title('Fourier transform')
+    plt.title('Pure FFT of ' + filename)
     plot_spectrum(new_image)
-    save_new(new_image, filename, './results/fft_')
+    plt.savefig('./results/pure_fft_' + filename + '.jpg')
 
-    inv = fft.irfft2(new_image)
     plt.figure()
-    plt.imshow(inv, cmap='gray')
+    plt.title('FFT of ' + filename + ' with FFTShift')
+    plot_spectrum(np.fft.fftshift(new_image))
+    plt.savefig('./results/shifted_fft_' + filename + '.jpg')
+
+    # save_new(np.abs(new_image), filename, './results/fft_')
+
+    inv = fft.ifft2(new_image)
+    plt.figure()
+    plt.imshow(np.abs(inv), cmap='gray')
     plt.title('Reconstructed Image')
+    plt.savefig('./buffer/reconstructed_' + filename + '.jpg')
+    # save_new(inv, filename, './results/inv_fft_')
     plt.show()
-    save_new(inv, filename, './results/inv_fft_')
 
 
 if __name__ == '__main__':
-    print('Input source file\' name here:')
-    src = 'BRIDGE.BMP'
+    print('Input source file\'s name here:')
+    src = 'jankaluza_dew_drop.jpg'
     run(src)
