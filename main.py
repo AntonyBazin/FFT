699a25d5d1744291f566242ade55b7dc5a825be3
Anton <antonyvbazin@gmail.com>
2020-11-21 20:20:59 +0300

    Refactored code, some improvements

--- main.py
+++ main.py
@@ -5,55 +5,71 @@ from matplotlib.colors import LogNorm
 import numpy as np
 
 
-def save_grayscale(image_name: str):
-    grayscale_image = Image.open(image_name).convert('L')
-    grayscale_image.save('./buffer/gray_' + image_name)
-    return './buffer/gray_' + image_name
+class Transformer:
+    def __init__(self, filename: str):
+        self.file = filename
+        self.name = filename[:-3]
+        self._data = None
+        self._plotting = None
 
+    def run(self):
+        self.prepare()
+        self.transform()
+        self.inverse()
+        plt.show()
 
-def save_new(pixels, filename, addition):
-    """Save a version of the image from the pixels array"""
-    image = Image.fromarray(pixels, 'L')
-    image.save(addition + filename)
+    def inverse(self):
+        self._data = fft.ifft2(self._data)
+        self._plotting = np.abs(self._data)
+        self.plot('Reconstructed image',
+                  'gray',
+                  False)
+        self.plot()
+        plt.savefig('./buffer/reconstructed_' + self.file + '.jpg',
+                    bbox_inches='tight',
+                    pad_inches=0)
 
+    def transform(self):
+        self._data = fft.fft2(self._data)  # may also use real fft here, but it will only give a half of the image
+        self._plotting = np.abs(self._data)
+        self.plot('Pure FT of ' + self.file,
+                  'gray',
+                  True,
+                  './results/pure_fft_',
+                  norm=LogNorm(vmin=1))
+        self._plotting = np.abs(np.fft.fftshift(self._data))
+        self.plot('FT of ' + self.file + ' with FFTShift',
+                  'gray',
+                  True,
+                  './results/shifted_fft_',
+                  norm=LogNorm(vmin=1))
 
-def plot_spectrum(im_fft):
-    """A logarithmic colormap"""
-    plt.imshow(np.abs(im_fft), cmap='gray', norm=LogNorm(vmin=1))
-    plt.colorbar()
+    def prepare(self):
+        grayscale_image = Image.open('./samples/' + self.file).convert('L')
+        grayscale_image.save('./buffer/gray_' + self.file)
 
+        self._data = plt.imread('./buffer/gray_' + self.file)
+        self._plotting = self._data
+        self.plot('Original image')
 
-def run(filename: str):
-    gray_img = save_grayscale(filename)
-
-    data = plt.imread(gray_img)  # 2d pixel array
-    plt.figure()
-    plt.imshow(data, cmap='gray')
-    plt.title('Original image')
-
-    new_image = fft.fft2(data)  # may also use real fft here, but it will only give a half of the image
-    plt.figure()
-    plt.title('Pure FFT of ' + filename)
-    plot_spectrum(new_image)
-    plt.savefig('./results/pure_fft_' + filename + '.jpg')
-
-    plt.figure()
-    plt.title('FFT of ' + filename + ' with FFTShift')
-    plot_spectrum(np.fft.fftshift(new_image))
-    plt.savefig('./results/shifted_fft_' + filename + '.jpg')
-
-    # save_new(np.abs(new_image), filename, './results/fft_')
-
-    inv = fft.ifft2(new_image)
-    plt.figure()
-    plt.imshow(np.abs(inv), cmap='gray')
-    plt.title('Reconstructed Image')
-    plt.savefig('./buffer/reconstructed_' + filename + '.jpg')
-    # save_new(inv, filename, './results/inv_fft_')
-    plt.show()
+    def plot(self, title='', colormap='gray', bar=False, save='', **kwargs):
+        plt.figure()
+        plt.title(title)
+        if kwargs:
+            plt.imshow(self._plotting, cmap=colormap, norm=kwargs.get('norm'))
+        else:
+            plt.imshow(self._plotting, cmap=colormap)
+        if bar:
+            plt.colorbar()
+        if save:
+            plt.savefig(save + self.name + '.jpg')
 
 
 if __name__ == '__main__':
     print('Input source file\'s name here:')
-    src = 'jankaluza_dew_drop.jpg'
-    run(src)
+    src = input()
+    worker = Transformer(src)
+    try:
+        worker.run()
+    except FileNotFoundError as er:
+        print(er)
