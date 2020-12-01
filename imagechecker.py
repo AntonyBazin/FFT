1ab866a59e53e1f93d0a263a528d993c7d86aa68
Anton <antonyvbazin@gmail.com>
2020-12-01 13:55:42 +0300

    Refactored the checker script

--- imagechecker.py
+++ imagechecker.py
@@ -1,35 +1,34 @@
 from transform import Transformer
 
 if __name__ == '__main__':
+    shapes = ['round.jpg', 'square.png', 'triangle.png']
+    for sh in shapes:
+        trans = Transformer(sh)
+        trans.transform().shift()
+        trans.plot_fft('Shifted FT of')
+    Transformer.show_all()
+
     tanks = ['tank1.jpg', 'tank2.jpg', 'tank3.jpg']
     planes = ['plane1.jpg', 'plane2.jpg', 'plane3.jpg']
-    # for tank, plane in zip(tanks, planes):
-    #     tank_tr = Transformer(tank)
-    #     plane_tr = Transformer(plane)
-    #     tank_tr.transform().shift()
-    #     plane_tr.transform().shift()
-    #     tank_tr.plot_fft(tank)
-    #     plane_tr.plot_fft(plane)
-    shapes = ['round.jpg', 'square.png', 'triangle.png']
-    # for sh in shapes:
-    #     trans = Transformer(sh)
-    #     trans.transform().shift()
-    #     trans.plot_fft(sh)
-    tr = Transformer('square.png')
-    lol = tr.transform()
-    lol.plot_fft('1st FT')
-    lol.shift()
-    lol.plot_fft('1st FT shifted')
-    tr.transform().shift()
-    tr.plot_fft('2nd shifted FT')
-    tr.transform().shift()
-    tr.plot_fft('3rd shifted FT')
-    tr.transform().shift()
-    tr.plot_fft('4th shifted FT')
-    tr.transform().shift()
-    tr.plot_fft('5th shifted FT')
-    tr.transform().shift()
-    tr.plot_fft('6th shifted FT')
-    tr.transform().shift()
-    tr.plot_fft('7th shifted FT')
+    for tank, plane in zip(tanks, planes):
+        tank_tr = Transformer(tank)
+        plane_tr = Transformer(plane)
+        tank_tr.transform().shift()
+        plane_tr.transform().shift()
+        tank_tr.plot_fft('Shifted FT of')
+        plane_tr.plot_fft('Shifted Ft of')
+    Transformer.show_all()
+
+    tr = Transformer('vase.jpg')
+    tr2 = Transformer('face.jpg')
+    tr.plot('Original vase')
+    tr2.plot('Original face')
+    tr.transform()
+    tr.plot_fft('Vase')
+    tr2.transform()
+    tr2.plot_fft('Face')
+    tr.shift()
+    tr.plot_fft('Shifted Vase')
+    tr2.shift()
+    tr2.plot_fft('Shifted Face')
     Transformer.show_all()
