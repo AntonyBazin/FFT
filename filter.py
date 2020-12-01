c61c8ff7eb7eda17f14b1ca968c60582469035a4
Anton <antonyvbazin@gmail.com>
2020-12-01 18:20:53 +0300

    The high pass and low pass filters are now working

--- filter.py
+++ filter.py
@@ -1,10 +1,12 @@
 from transform import Transformer
 import numpy as np
+import sys
+
 
 # remove noise from images using FFT
 
 
-class Denoiser(Transformer):
+class Filter(Transformer):
     def __init__(self, filename: str, frac=0.1):
         super().__init__(filename)
         self.fraction = frac
@@ -14,9 +16,9 @@ class Denoiser(Transformer):
     def method_disabled():
         raise NotImplementedError('This function is not implemented in denoiser')
 
-    def transform(self): Denoiser.method_disabled()
+    def transform(self): Filter.method_disabled()
 
-    def inverse(self): Denoiser.method_disabled()
+    def inverse(self): Filter.method_disabled()
 
     def filter_many_times(self, fractions: list):
         super().transform()
@@ -30,21 +32,60 @@ class Denoiser(Transformer):
                       'gray',
                       False)
 
-    def filter_spectrum(self):
+    def low_pass_filter(self):
         super().transform()
         self._data[int(self.r * self.fraction):int(self.r * (1 - self.fraction))] = 0
         self._data[:, int(self.c * self.fraction):int(self.c * (1 - self.fraction))] = 0
         self._plotting = np.abs(self._data)
-        # self.plot_fft('Filtered Spectrum')
+        self.plot_fft('Low pass filtered spectrum')
+        self.shift()
+        self.plot_fft('Low pass filtered spectrum with shift')
         super().inverse()
-        self.plot('Reconstructed image',
+        self.plot('Low pass filtered image',
                   'gray',
                   False)
 
+    def high_pass_filter(self):
+        super().transform()
+        self._data[0:int(self.r * self.fraction), 0:int(self.c * self.fraction)] = 0
+        self._data[int(self.r * (1 - self.fraction)):self.r, 0:int(self.c * self.fraction)] = 0
+        self._data[0:int(self.r * self.fraction), int(self.c * (1 - self.fraction)):self.c] = 0
+        self._data[int(self.r * (1 - self.fraction)):self.r, int(self.c * (1 - self.fraction)):self.c] = 0
+        self._plotting = np.abs(self._data)
+        self.plot_fft('High pass filtered spectrum')
+        self.shift()
+        self.plot_fft('High pass filtered spectrum with shift')
+        super().inverse()
+        self.plot('High pass filtered image',
+                  'gray',
+                  False)
+
+    # def low_pass_filter(self):
+    #     super().transform()
+    #     n = np.amax(self._data)
+    #     print(n)
+    #     self._data[0:int(self.r * self.fraction), 0:int(self.c * self.fraction)] = n
+    #     self._data[int(self.r * (1 - self.fraction)):self.r, 0:int(self.c * self.fraction)] = n
+    #     self._data[0:int(self.r * self.fraction), int(self.c * (1 - self.fraction)):self.c] = n
+    #     self._data[int(self.r * (1 - self.fraction)):self.r, int(self.c * (1 - self.fraction)):self.c] = n
+    #     self._plotting = np.abs(self._data)
+    #     self.plot_fft('Filtered Spectrum')
+    #     super().inverse()
+    #     self.plot('Low pass filtered image',
+    #               'gray',
+    #               False)
+
 
 if __name__ == '__main__':
-    # dn = Denoiser('noise_city.jpg')
+    # dn = Filter('noise_city.jpg')
     # dn.filter_many_times([0.07, 0.08, 0.09, 0.1, 0.11, 0.15])
-    dn2 = Denoiser('moonlanding.png')
-    dn2.filter_many_times([0.07, 0.08, 0.09, 0.1, 0.11, 0.15])
+    # dn2 = Filter('moonlanding.png')
+    # dn2.filter_many_times([0.07, 0.08, 0.09, 0.1, 0.11, 0.15])
+    fl1 = Filter('6A(142).BMP', 0.07)
+    fl1.plot('6A(142).BMP')
+    fl1.low_pass_filter()
+
+    fl = Filter('6A(142).BMP', 0.07)
+    fl.high_pass_filter()
+    # fl.high_pass_filter()
     Transformer.show_all()
