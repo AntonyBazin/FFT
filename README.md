1ecc8b4f25ee06b70ecb4bb33191d7f362ce53a2
Anton <antonyvbazin@gmail.com>
2020-12-03 23:55:30 +0300

    Added the timing testing, and the directories for dummy files. Added plotting the timing results instructions to README

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 Some experiments with the Fast Fourier Transform.
 
 [More about FFT](https://en.wikipedia.org/wiki/Fast_Fourier_transform "Wiki")
+
+
+## Timing
+
+You can use 
+
+```bash
+gnuplot --persist -e 'plot "timing-fft.txt" u 1:2 w linespoints linestyle 1'
+```
+or
+```bash
+gnuplot --persist -e 'set logscale x; plot "timing-fft.txt" using 1:2 with linespoints'
+```
+to plot the timing diagrams.
---
+++ timing.py
@@ -0,0 +1,34 @@
+from transform import Transformer
+import time
+import sys
+
+
+if __name__ == '__main__':
+    try:
+        screwdriver = Transformer('6A(142).BMP')  # 320x240 = 76800 p
+        vase = Transformer('vase.jpg')  # 320x400 = 128000 p
+        duss = Transformer('DUSS.BMP')  # 672x473 = 317856 p
+        round_tr = Transformer('round.jpg')  # 800x800 = 640000 p
+        dew = Transformer('jankaluza_dew_drop.jpg')  # 3840x2562 = 9838080 p
+        fire = Transformer('vovalente_fire.jpg')  # 5077x3385 = 17185645 p
+    except FileNotFoundError as er:
+        print(er)
+        sys.exit()
+    with open('timing-fft.txt', 'w') as out:
+        out.write('# number TIME\n')
+    with open('timing-shift.txt', 'w') as out:
+        out.write('# number TIME\n')
+    transformers = [screwdriver, vase, duss, round_tr, dew, fire]
+    sizes = [768, 128000, 317856, 640000, 9838080, 17185645]
+    for size, transformer in zip(sizes, transformers):
+        start = time.time()
+        transformer.transform()
+        end = time.time()
+        with open('timing-fft.txt', 'a') as log:
+            log.write(str(size) + ' ' + str(end - start) + '\n')
+    for size, transformer in zip(sizes, transformers):
+        start = time.time()
+        transformer.shift()
+        end = time.time()
+        with open('timing-shift.txt', 'a') as log:
+            log.write(str(size) + ' ' + str(end - start) + '\n')
