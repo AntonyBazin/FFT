be00d9b118e19603ebf2d8ec48caac84216e42a0
Anton <antonyvbazin@gmail.com>
2020-12-12 14:35:13 +0300

    Added a simple gui interface

--- comparison.py
+++ comparison.py
@@ -10,17 +10,6 @@ if __name__ == '__main__':
         trans.plot_fft('Shifted FT of')
     Transformer.show_all()
 
-    # tanks = ['tank1.jpg', 'tank2.jpg', 'tank3.jpg']
-    # planes = ['plane1.jpg', 'plane2.jpg', 'plane3.jpg']
-    # for tank, plane in zip(tanks, planes):
-    #     tank_tr = Transformer(tank)
-    #     plane_tr = Transformer(plane)
-    #     tank_tr.transform().shift()
-    #     plane_tr.transform().shift()
-    #     tank_tr.plot_fft('Shifted FT of')
-    #     plane_tr.plot_fft('Shifted Ft of')
-    # Transformer.show_all()
-
     tr = Transformer('vase.jpg')
     tr2 = Transformer('face.jpg')
     tr.plot('Original vase')
---
+++ main.py
@@ -0,0 +1,160 @@
+import filter as fl
+from transform import Transformer
+import time
+import sys
+import tkinter as tk
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        master.title('Main dialog')
+
+        no_file_label = tk.Label(self, text='No input is needed to run these:')
+        run_show_primitives = tk.Button(self, text='Run primitives transform',
+                                        command=self.run_show_primitives)
+        run_comp = tk.Button(self, text='Run face and vase spectra comparison',
+                                        command=self.run_comp)
+        run_timing_button = tk.Button(self, text='Run timing',
+                                      command=self.run_timing)
+
+        file_label = tk.Label(self, text='Functions below need a file to work on:')
+        self.box = tk.Entry(self, textvariable=tk.StringVar)
+        self.box.insert(0, 'File\'s name')
+        run_transform_button = tk.Button(self, text='Run fft and inverse fft',
+                                         command=self.run_transform)
+        file_and_frac_label = tk.Label(self,
+                                       text='Different filters function needs a fraction to work with:')
+        self.fracbox = tk.Entry(self, textvariable=tk.StringVar)
+        self.fracbox.insert(0, '0.1')
+        run_filter_button = tk.Button(self, text='Run different filters',
+                                      command=self.run_filter)
+
+        fraction_list_label = tk.Label(self,
+                                       text='Filtering with many fractions requires a list to work with:')
+        self.many_fractions = tk.Entry(self, textvariable=tk.StringVar)
+        self.many_fractions.insert(0, '0.05 0.07 0.1 0.2 0.3')
+        run_list_filter_button = tk.Button(self, text='Run filter with list',
+                                           command=self.run_list_filter)
+
+        quit_button = tk.Button(self, text="QUIT", fg="red",
+                                command=self.master.destroy)
+
+        self.items = [no_file_label,
+                      run_comp,
+                      run_show_primitives,
+                      run_timing_button,
+                      file_label, self.box,
+                      run_transform_button,
+                      file_and_frac_label,
+                      self.fracbox,
+                      run_filter_button,
+                      fraction_list_label,
+                      self.many_fractions,
+                      run_list_filter_button,
+                      quit_button]
+        self.create_widgets()
+
+    def create_widgets(self):
+        for item in self.items:
+            item.pack()
+
+    def run_transform(self):
+        name = self.box.get()
+        try:
+            worker = Transformer(name)
+            worker.run()
+        except FileNotFoundError as er:
+            print(er)
+
+    def run_filter(self):
+        name = self.box.get()
+        try:
+            fraction = float(self.fracbox.get())
+            fl1 = fl.Filter(name, fraction)
+            fl1.plot(name)
+            fl1.low_pass_filter()
+
+            fl2 = fl.Filter(name, fraction)
+            fl2.high_pass_filter()
+            Transformer.show_all()
+        except Exception as er:
+            print(er)
+
+    def run_list_filter(self):
+        name = self.box.get()
+        try:
+            fractions = self.many_fractions.get().split()
+            fractions = [float(fraction) for fraction in fractions]
+            fil = fl.Filter(name)
+            fil.plot(name)
+            fil.series_lpf(fractions)
+            fl.Transformer.show_all()
+        except Exception as er:
+            print(er)
+
+    @staticmethod
+    def run_show_primitives():
+        shapes = ['round.jpg', 'square.png', 'triangle.png']
+        for sh in shapes:
+            trans = Transformer(sh)
+            trans.plot()
+            trans.transform().shift()
+            trans.plot_fft('Shifted FT of')
+        Transformer.show_all()
+
+    @staticmethod
+    def run_comp():
+        tr = Transformer('vase.jpg')
+        tr2 = Transformer('face.jpg')
+        tr.plot('Original vase')
+        tr2.plot('Original face')
+        tr.transform()
+        tr.plot_fft('FT')
+        tr2.transform()
+        tr2.plot_fft('FT')
+        tr.shift()
+        tr.plot_fft('Shifted FT')
+        tr2.shift()
+        tr2.plot_fft('Shifted FT')
+        Transformer.show_all()
+
+    @staticmethod
+    def run_timing():
+        try:
+            screwdriver = Transformer('6A(142).BMP')  # 320x240 = 76800 p
+            vase = Transformer('vase.jpg')  # 320x400 = 128000 p
+            city = Transformer('DUSS.BMP')  # 672x473 = 317856 p
+            round_tr = Transformer('round.jpg')  # 800x800 = 640000 p
+            dew = Transformer('jankaluza_dew_drop.jpg')  # 3840x2562 = 9838080 p
+            fire = Transformer('vovalente_fire.jpg')  # 5077x3385 = 17185645 p
+        except FileNotFoundError as er:
+            print(er)
+            sys.exit()
+        with open('timing-fft.txt', 'w') as out:
+            out.write('# number TIME\n')
+        with open('timing-shift.txt', 'w') as out:
+            out.write('# number TIME\n')
+        transformers = [screwdriver, vase, city, round_tr, dew, fire]
+        sizes = [76800, 128000, 317856, 640000, 9838080, 17185645]
+        for size, transformer in zip(sizes, transformers):
+            start = time.time()
+            transformer.transform()
+            end = time.time()
+            with open('timing-fft.txt', 'a') as log:
+                log.write(str(size) + ' ' + str(end - start) + '\n')
+        for size, transformer in zip(sizes, transformers):
+            start = time.time()
+            transformer.shift()
+            end = time.time()
+            with open('timing-shift.txt', 'a') as log:
+                log.write(str(size) + ' ' + str(end - start) + '\n')
+        print('Timing completed successfully')
+
+
+root = tk.Tk()
+root.geometry('500x500')
+app = Application(master=root)
+app.mainloop()
